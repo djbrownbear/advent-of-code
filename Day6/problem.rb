43e43886869dc3f8761cd826952a3ed48566fa51
Aaron <41420965+djbrownbear@users.noreply.github.com>
2022-12-11 00:02:18 -0800

    feat: finish pt 1

--- Day6/problem.rb
+++ Day6/problem.rb
@@ -1,4 +1,19 @@
-f = File.read("test_input")
+f = File.read("input")
 g = f.split("\r\n")
 
-p g
+def get_uniq_group_index(arr, n)
+  (0...arr.length-n-1).each do |i|
+    group = arr[i..i+n-1]
+    return i+n if group.chars.uniq.count == n
+  end
+end
+
+p "PART 1"
+g.each do |line|
+  p "#{get_uniq_group_index(line, 4)} : #{line}"
+end
+
+p "PART 2"
+g.each do |line|
+  p "#{get_uniq_group_index(line, 14)} : #{line}"
+end
